01693d1be1677884532353b69fbd11b98e386840
Lordcreos <leonardosanchez4h@hotmail.com>
2021-01-05 18:30:47 -0500

    CP commiteado por lordcreos a las 1609889446979

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,23 +3,27 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array [0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array [array.length-1];
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 function incrementarPorUno(array) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
-  // Tu código:
+  // Tu código:ls
+  
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
